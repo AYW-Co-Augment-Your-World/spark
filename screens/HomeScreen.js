2116e614334f599634df8dfd78e68600d0449102
Dominique <dominiqueboyer94@yahoo.com>
2019-12-07 12:55:16 -0800

    added to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -52,6 +52,7 @@ export default class HomeScreen extends React.Component{
             <Text style={styles.getStartedText}> Welcome {this.state.email}!</Text>
 
             <Text>Please make sure that you keep your profile up to date! Please!</Text>
+            <Text>Please make sure that you keep your profile up to date! Please!</Text>
 
             <Text style={styles.getStartedText}>
               I am changing this text because i want to see a change. Lets see if it works now
